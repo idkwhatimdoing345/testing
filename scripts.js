a5c8e59e97d7cc7967163c6a5db74d2a95e24898
idkwhatimdoing345 <samuel.hirsh@outlook.com>
2025-02-20 12:43:18 -0600

    update app

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-alert("this is a test")
+let button = document.getElementById("clicker")
+let score = 0
+let textdisplay = document.getElementById("TDS")
+function update(){
+    score += 1
+    textdisplay.textContent = "points: " + score
+}
+button.addEventListener("click", update)

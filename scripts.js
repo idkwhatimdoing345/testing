a7a789f2eda89508625a376d79bd72a8894f6fd7
idkwhatimdoing345 <samuel.hirsh@outlook.com>
2025-02-20 13:29:04 -0600

    bug patch

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ let score = 0
 let textdisplay = document.getElementById("TDS")
 let skval = document.getElementById("skipcount")
 function update(){
-    score += skval.value
+    score += parseInt(skval.value)
     textdisplay.textContent = "points: " + score
 }
 button.addEventListener("click", update)

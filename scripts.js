7b560baff4dd8b848516fadaac256f95c13a81b3
idkwhatimdoing345 <samuel.hirsh@outlook.com>
2025-02-20 13:25:57 -0600

    pushout update

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
 let button = document.getElementById("clicker")
 let score = 0
 let textdisplay = document.getElementById("TDS")
+let skval = document.getElementById("skipcount")
 function update(){
-    score += 1
+    score += skval.value
     textdisplay.textContent = "points: " + score
 }
 button.addEventListener("click", update)

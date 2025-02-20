326849a729fe3d6acce4a850633b171d327b188a
idkwhatimdoing345 <samuel.hirsh@outlook.com>
2025-02-20 13:33:34 -0600

    update

--- scripts.js
+++ scripts.js
@@ -2,8 +2,14 @@ let button = document.getElementById("clicker")
 let score = 0
 let textdisplay = document.getElementById("TDS")
 let skval = document.getElementById("skipcount")
+let clearb = document.getElementById("clear")
 function update(){
     score += parseInt(skval.value)
     textdisplay.textContent = "points: " + score
 }
 button.addEventListener("click", update)
+function clear(){
+    score = 0
+    update
+}
+clearb.addEventListener("click", clear)

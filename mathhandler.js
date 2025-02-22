0186062ee854841f172f02d120bd6bf4824627a1
idkwhatimdoing345 <samuel.hirsh@outlook.com>
2025-02-21 18:20:32 -0600

    d

--- mathhandler.js
+++ mathhandler.js
@@ -15,30 +15,30 @@ var finval = 0;
 
 // Function to update the displayed result
 function updateResult() {
-    result.innerText = "Result: " + finval;
+  result.innerText = "Result: " + finval;
 }
 
 // Toggle add menu and close subtract menu
 button1.addEventListener("click", function () {
-    menuopt1.hidden = !menuopt1.hidden; // Toggle add menu
-    menuopt2.hidden = true; // Hide subtract menu
+  menuopt1.hidden = !menuopt1.hidden; // Toggle add menu
+  menuopt2.hidden = true; // Hide subtract menu
 });
 
 // Toggle subtract menu and close add menu
 button2.addEventListener("click", function () {
-    menuopt2.hidden = !menuopt2.hidden; // Toggle subtract menu
-    menuopt1.hidden = true; // Hide add menu
+  menuopt2.hidden = !menuopt2.hidden; // Toggle subtract menu
+  menuopt1.hidden = true; // Hide add menu
 });
 
 // Increment the value based on input
 inc.addEventListener("click", function () {
-    let addNum = parseFloat(addvalue.value) || 0; // Get the add input value
-    let subNum = parseFloat(subvalue.value) || 0; // Get the subtract input value
+  let addNum = parseFloat(addvalue.value) || 0; // Get the add input value
+  let subNum = parseFloat(subvalue.value) || 0; // Get the subtract input value
 
-    finval += addNum; // Add the input value
-    finval -= subNum; // Subtract the input value
+  finval += addNum; // Add the input value
+  finval -= subNum; // Subtract the input value
 
-    updateResult(); // Update the result display
+  updateResult(); // Update the result display
 });
 
 // Initialize result display
